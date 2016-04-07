9882f1152cc7e4a01621544b7d0f5bcd85e10fee
Samiha Akram <samiha.angel24@gmail.com>
2016-04-07 10:50:15 +0600

    Added files via upload

--- Uri 1011(sphere).c
+++ Uri 1011(sphere).c
@@ -1,21 +1,13 @@
 #include<stdio.h>
-
 #define PI 3.14159
-
 int main()
-
 {
 
     int radius;
-    
     double volume;
-    
     scanf("%d",&radius);
-    
-    volume = (4.0*PI*radius*radius*radius)/3;
-    
+    volume=(4.0*PI*radius*radius*radius)/3;
     printf("VOLUME = %0.3lf\n",volume);
-    
     return 0;
 
 }
