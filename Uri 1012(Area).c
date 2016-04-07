48ec1e55f3a543515369187f8b8cec22dbe2d5d5
Samiha Akram <samiha.angel24@gmail.com>
2016-04-07 10:51:14 +0600

    Added files via upload

---
+++ Uri 1012(Area).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define pi 3.14159
+int main()
+{
+    float A,B,C,triangle,circle,trapizium,square,rectangle;
+    scanf("%f %f %f",&A,&B,&C);
+    triangle = 0.5*A*C;
+    circle = pi*C*C;
+    trapizium = 0.5*(A+B)*C;
+    square = B*B;
+    rectangle = A*B;
+    printf("TRIANGULO: %0.3f\nCIRCULO: %0.3f\nTRAPEZIO: %0.3f\nQUADRADO: %0.3f\nRETANGULO: %0.3f\n",triangle,circle,trapizium,square,rectangle);
+
+
+    return 0;
+}
