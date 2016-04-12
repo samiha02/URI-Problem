5351d62c63823c74c8a8936dc27765624c885e06
Samiha Akram <samiha.angel24@gmail.com>
2016-04-12 19:41:22 +0600

    Added files via upload

---
+++ Uri 1038(snack).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+
+    int X,Y;
+    float Z;
+    scanf("%d %d",&X,&Y);
+    if(X==1){
+        Z =Y * 4.00;
+        printf("Total: R$ %0.2f\n",Z);
+    }
+     if(X==2){
+        Z =Y * 4.50;
+        printf("Total: R$ %0.2f\n",Z);
+    }
+     if(X==3){
+        Z =Y * 5.00;
+        printf("Total: R$ %0.2f\n",Z);
+    }
+     if(X==4){
+        Z =Y * 2.00;
+        printf("Total: R$ %0.2f\n",Z);
+    }
+     if(X==5){
+        Z =Y * 1.50;
+        printf("Total: R$ %0.2f\n",Z);
+    }
+     return 0;
+}
