6de16baf83a213fb38d4b22fa994cf4b15b9a860
Samiha Akram <samiha.angel24@gmail.com>
2016-04-15 23:48:07 +0600

    Added files via upload

---
+++ Uri 1065(Even Between five Numbers).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,count = 0;
+
+    for(i=1 ; i<=5 ; i++){
+
+        scanf("%d",&a);
+        if(a%2==0){
+            count++;
+        }
+    }
+    printf("%d valores pares\n",count);
+
+    return 0;
+}
