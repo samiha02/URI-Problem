e4ebe6145d68d105f325211e98b8c2bf105a93bb
Samiha Akram <samiha.angel24@gmail.com>
2016-04-22 17:51:56 +0600

    Added files via upload

---
+++ Uri 1113(Ascending and Descending).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+
+    while(scanf("%d %d",&x,&y)==2){
+
+    if(x>y)
+
+        printf("Decrescente\n");
+
+    else if(x<y)
+
+        printf("Crescente\n");
+
+    else if(x==y)
+
+        break;
+    }
+    return 0;
+}
